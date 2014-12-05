e26294c4dfc7db98735b87e18f62ee8fa7c06b63
Steven <shicks1000@yahoo.com>
2014-12-05 16:13:01 +0100

    Added functionality to unit selection

--- Android/app/src/main/java/com/example/uia/battlefree/ChooseUnits.java
+++ Android/app/src/main/java/com/example/uia/battlefree/ChooseUnits.java
@@ -6,63 +6,144 @@ import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.support.v4.app.Fragment;
 
 import java.util.ArrayList;
 
 
-public class ChooseUnits extends Activity {
+public class ChooseUnits extends FragmentActivity {
+
+    Button CU1;
+    Button CU2;
+    Button CU3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_units);
+        FragmentManager fm = getSupportFragmentManager();
+         Fragment fragment = fm.findFragmentById(R.id.fragment_container);
 
-        final Button chooseUnit1 = (Button) findViewById(R.id.unit_button_1);
-        final Button chooseUnit2 = (Button) findViewById(R.id.unit_button_2);
-        final Button chooseUnit3 = (Button) findViewById(R.id.unit_button_3);
-        final Button chooseUnit4 = (Button) findViewById(R.id.unit_button_4);
-        final Button chooseUnit5 = (Button) findViewById(R.id.unit_button_5);
-        final Button chooseUnit6 = (Button) findViewById(R.id.unit_button_6);
 
-        chooseUnit1.setOnClickListener(new View.OnClickListener() {
+        final Button orc = (Button) findViewById(R.id.chooseOrc);
+        final Button skeleton = (Button) findViewById(R.id.chooseSkeleton);
+        final Button supplycart= (Button) findViewById(R.id.chooseSupplyCart);
+        final Button thief = (Button) findViewById(R.id.chooseThief);
+        final Button vampire = (Button) findViewById(R.id.chooseVampire);
+
+        CU1 = (Button) findViewById(R.id.chosen_unit_1);
+        CU2 = (Button) findViewById(R.id.chosen_unit_2);
+        CU3 = (Button) findViewById(R.id.chosen_unit_3);
+
+        CU1.setVisibility(View.INVISIBLE);
+        CU2.setVisibility(View.INVISIBLE);
+        CU3.setVisibility(View.INVISIBLE);
+
+        orc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                setButtonBackground(chooseUnit1, view);
+
+                startActivity(new Intent(ChooseUnits.this, UnitDescriptionFragment.class));
+
+                if(isPicked(orc)){
+                    System.out.println("already picked");
+                }else{
+                    chooseUnit(orc);
+                }
+        }
+        });
+        skeleton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chooseUnit(skeleton);
+            }
+        });
+        supplycart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chooseUnit(supplycart);
+            }
+        });
+        thief.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chooseUnit(thief);
+            }
+        });
+        vampire.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chooseUnit(vampire);
+            }
+        });
+
+        CU1.setOnClickListener(new View.OnClickListener(){
+           @Override
+           public void onClick(View view){
+               if(CU1.getVisibility() == View.VISIBLE){
+                   CU1.setVisibility(View.INVISIBLE);
+                   CU1.setBackground(null);
+               }
+           }
+        });
+        CU2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                if(CU2.getVisibility() == View.VISIBLE){
+                    CU2.setVisibility(View.INVISIBLE);
+                    CU2.setBackground(null);
+                }
+            }
+        });
+        CU3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                if(CU3.getVisibility() == View.VISIBLE){
+                    CU3.setVisibility(View.INVISIBLE);
+                    CU3.setBackground(null);
+                }
             }
         });
     }
 
-    public void setButtonBackground(Button b, View v) {
-        final Button CU1 = (Button) findViewById(R.id.chosen_unit_1);
-        final Button CU2 = (Button) findViewById(R.id.chosen_unit_2);
-        final Button CU3 = (Button) findViewById(R.id.chosen_unit_3);
-        TextView t = (TextView)findViewById(R.id.textView2);
+    public void chooseUnit(Button chosenUnit) {
+        Drawable chosenUnitBackground = chosenUnit.getBackground();
+        ArrayList<Button> bArray = getChosenList();
 
-
-
-        Drawable chosenUnitBackground = b.getBackground();
-
-        ArrayList<Button> bArray = new ArrayList<Button>();
-        bArray.add(CU1);
-        bArray.add(CU2);
-        bArray.add(CU3);
-
-        for (int i = 0; i < bArray.size(); i++) {
-            Button chosenUnit = bArray.get(i);
-            int visibility = chosenUnit.getVisibility();
-            System.out.println(visibility);
-            t.setText(""+visibility);
-            if (visibility == 4) {
-                chosenUnit.setBackground(getResources().getDrawable(R.drawable.orcbigger));
-                chosenUnit.setVisibility(View.VISIBLE);
+        int i = 0;
+        for(Button m : bArray){
+            if(m.getVisibility() == View.INVISIBLE && i == 0){
+                m.setBackground(chosenUnitBackground);
+                m.setVisibility(View.VISIBLE);
+                i++;
             }
         }
         bArray.clear();
+    }
+
+    public ArrayList<Button> getChosenList(){
+        ArrayList<Button> chosenList = new ArrayList<Button>();
+        chosenList.add(CU1);
+        chosenList.add(CU2);
+        chosenList.add(CU3);
+        return chosenList;
+    }
+
+    public boolean isPicked(Button pushedUnit){
+        boolean picked = false;
+        for(Button chosen : getChosenList()){
+            if(chosen.getBackground() == pushedUnit.getBackground()){
+                picked=true;
+            }
+        }
+        return picked;
     }
 
     @Override
